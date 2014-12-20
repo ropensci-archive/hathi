a6e6f134fb825ea5aee2d56332f2ae107031fa06
Scott Chamberlain <myrmecocystus@gmail.com>
2014-12-19 17:47:12 -0800

    added hathi_data man file, playing with data endpt, no progress really

--- R/hathi_data.r
+++ R/hathi_data.r
@@ -5,41 +5,49 @@
 # your debt below 50 within 30 seconds.
 
 # 'http[s]://babel.hathitrust.org/cgi/htd/:RESOURCE/:ID[/:FILEID|:SEQ][?:QUERY_STRING]'
-# 
+
 # library('httr')
 # res <- GET('https://babel.hathitrust.org/cgi/htd/structure/mdp?format=xml&v=2&')
 # stop_for_status(res)
-# 
-# res <- GET('https://babel.hathitrust.org/htd/volume/pageocr/dul1.ark:/13960/t00z82c1q/14')
+# content(res)
+#
+# res <- GET('https://babel.hathitrust.org/htd/volume/pageocr/dul1.ark:/13960/t00z82c1q/14',
+#            query=list(oauth_consumer_key=key, oauth_consumer_secret=secret))
 # stop_for_status(res)
-# 
-# hathitrust_accesskey = "e7e0665525"
-# hathitrust_secretkey = "0e3cb11e1cd1dcb98fa4c0085be8"
-# getOption('hathitrust_accesskey')
-# getOption('hathitrust_secretkey')
-# 
-# 
-# github_auth <- function(appname=getOption('gh_appname'), key=getOption('gh_id'), secret=getOption('gh_secret')){
-#   if(is.null(getOption('gh_token'))){
-#     myapp <- oauth_app(appname, key, secret)
-#     token <- oauth2.0_token(oauth_endpoints('github'), myapp)
-#     options(gh_token = token)
-#   } else { token <- getOption('gh_token') }
-#   return( token )
-# }
-# 
-# oauth_endpoints("twitter")
-# 
-# # 2. Register an application at https://apps.twitter.com/
-# #    Insert your values below - if secret is omitted, it will look it up in
-# #    the TWITTER_CONSUMER_SECRET environmental variable.
-# myapp <- oauth_app("twitter", key = "TYrWFPkFAkn4G5BbkWINYw")
-# 
-# # 3. Get OAuth credentials
-# twitter_token <- oauth1.0_token(oauth_endpoints("twitter"), myapp)
-# 
-# # 4. Use API
-# req <- GET("https://api.twitter.com/1.1/statuses/home_timeline.json",
-#            config(token = twitter_token))
+#
+# # hathitrust_accesskey = "e7e0665525"
+# # hathitrust_secretkey = "0e3cb11e1cd1dcb98fa4c0085be8"
+# key <- getOption('hathitrust_accesskey')
+# secret <- getOption('hathitrust_secretkey')
+# endpts <- oauth_endpoint(request = "request_url",
+#                          authorize = "authorize",
+#                          access = "access",
+#                          base_url = "http://babel.hathitrust.org/cgi/htd")
+# hathiapp <- oauth_app("hathi", key = key, secret = secret)
+# token <- oauth1.0_token(endpts, myapp)
+# req <- GET("https://babel.hathitrust.org/htd/volume/pageocr/dul1.ark:/13960/t00z82c1q/14",
+#            config(token = token))
 # stop_for_status(req)
 # content(req)
+
+
+#' Hathitrust data collection.
+#'
+#' @export
+#'
+#' @details Details:
+#'
+#' @section Authentication:
+#' Get a key at \url{http://babel.hathitrust.org/cgi/kgs/request}
+#'
+#' @section More data:
+#' You can go to their web interface to generate datasets for later download. But wouldn't you
+#' rather do things programatically in R?
+#'
+#' @references \url{http://www.hathitrust.org/documents/hathitrust-data-api-v2.pdf}
+#' @examples \dontrun{
+#' hathi_data()
+#' }
+hathi_data <- function(){
+  message("nothin' yet")
+}
